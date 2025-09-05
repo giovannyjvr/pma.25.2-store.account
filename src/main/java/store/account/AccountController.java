9ad11302cc9ba9bbabee1b5a0ab85ba648d3f0ed
giovannyjvr <giovannyjvrusso@gmail.com>
2025-09-05 10:37:38 -0300

    em dia com as aulas

--- src/main/java/store/account/AccountController.java
+++ src/main/java/store/account/AccountController.java
@@ -2,26 +2,30 @@ package store.account;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AccountController {
-
+    
     @PostMapping("/account")
-    public AccountOut create(AccountIn in);
+    public ResponseEntity<AccountOut> create(
+        @RequestBody AccountIn in
+    );
 
     @GetMapping("/account/{id}")
-    public AccountOut findById(
+    public ResponseEntity<AccountOut> findById(
         @PathVariable("id") String id
     );
 
     @GetMapping("/account")
-    public List<AccountOut> findAll();
+    public ResponseEntity<List<AccountOut>> findAll();
 
     @DeleteMapping("/account/{id}")
-    public Void delete(
+    public ResponseEntity<Void> delete(
         @PathVariable("id") String id
     );
 
